e71c8ebe2dfd979c39b67acbd368354fb491d9d2
Panagiotis Astithas <pastith@gmail.com>
2010-11-20 21:15:59 +0200

    More elaborate parsing for event date.

--- api.js
+++ api.js
@@ -80,6 +80,18 @@ var pollSingleSource = function (sourceIndex) {
                     pubDate: pubDate
                 };
 
+                // Generate event time.
+                var dateElems = eventDate.split('/');
+                console.log('dateElems: ' + dateElems);
+                var timeElems = eventTime.split('-');
+                console.log('timeElems: ' + timeElems);
+                var startTimelems, start;
+                startTimeElems = timeElems[0].split(':');
+                console.log('startTimeElem[0]: ' + isNaN(startTimeElems[0]));
+                if (isNaN(startTimeElems[0]))
+                    start = new Date(dateElems[2], dateElems[1], dateElems[0]);
+                else
+                    start = new Date(dateElems[2], dateElems[1], dateElems[0], startTimeElems[0], startTimeElems[1]);
                 // Generate iCalendar version.
                 var event = {};
                 event.url = link;
@@ -89,8 +101,8 @@ var pollSingleSource = function (sourceIndex) {
                 event.description = newItem.description;
                 //event.location = newItem.;
                 //event.recurrence = newItem.;
-                event.start = newItem.pubDate;//newItem.date;
-                //event.end = newItem.;
+                event.start = start; //newItem.pubDate;
+                //event.end = (end && !isNaN(end)) || '';
                 result = createIcal(event);
                 console.log(result);
             }
