dc8f68ee4eda47e96efc8e69146851ccf3ec1246
Panagiotis Astithas <pastith@gmail.com>
2010-11-20 12:28:39 +0200

    Add server launcher.

---
+++ api.js
@@ -0,0 +1,4 @@
+// The router for the api requests.
+var router = exports.router = function (app) {
+};
+
---
+++ server.js
@@ -0,0 +1,28 @@
+var connect = require('connect'),
+	api = require('./api'),
+	port = parseInt(process.env.PORT) || 8000;
+
+var server = connect.createServer(
+	connect.logger(),
+	connect.errorHandler({ dumpExceptions: true, showStack: true })
+);
+// Serve static resources.
+server.use("/",
+    connect.conditionalGet(),
+    connect.cache(),
+    connect.gzip(),
+	connect.staticProvider(__dirname + '/public')
+);
+// Serve the API responses.
+server.use("/api",
+	connect.bodyDecoder(),
+	connect.cookieDecoder(),
+	connect.router(api.router)
+);
+server.listen(port);
+console.log('Eventharvy server running at http://127.0.0.1:' + port);
+
+process.addListener('uncaughtException', function (err) {
+	console.log(err.stack);
+});
+
