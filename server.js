a4581af2434cda6563e8953b7c01b5c4ea050b60
Christos Stathis <chstath@gmail.com>
2010-11-20 21:16:45 +0200

    First attempt with express

--- server.js
+++ server.js
@@ -1,25 +1,59 @@
-var connect = require('connect'),
+var express = require('express'),
 	api = require('./api'),
-	port = parseInt(process.env.PORT) || 8000;
+	port = parseInt(process.env.PORT) || 8000,
+    connect = require('connect');
 
-var server = connect.createServer(
-	connect.logger(),
-	connect.errorHandler({ dumpExceptions: true, showStack: true })
-);
-// Serve static resources.
-server.use("/",
-    connect.conditionalGet(),
-    connect.cache(),
-    connect.gzip(),
-	connect.staticProvider(__dirname + '/public')
-);
-// Serve the API responses.
-server.use("/api",
-	connect.bodyDecoder(),
-	connect.cookieDecoder(),
-	connect.router(api.router)
-);
-server.listen(port);
+var app = module.exports = express.createServer();
+
+// Configuration
+
+app.configure(function(){
+    app.set('views', __dirname + '/views');
+    app.use(connect.bodyDecoder());
+    app.use(connect.methodOverride());
+    app.use(connect.compiler({ src: __dirname + '/public', enable: ['less'] }));
+    app.use(app.router);
+    app.use(connect.staticProvider(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+    app.use(connect.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure('production', function(){
+   app.use(connect.errorHandler()); 
+});
+
+// Routes
+
+app.get('/', function(req, res){
+    res.render('index.jade', {
+        locals: {
+            title: 'Express'
+        }
+    });
+});
+
+app.get('/fetch', function (req, res) {
+    res.render('fetch.jade', {
+        locals: {
+            title: 'Feed',
+            body:'lalalal'
+        }
+    });
+});
+
+// Only listen on $ node app.js
+
+if (!module.parent) app.listen(port);
+
+
+
+
+
+
+
+
 console.log('Eventharvy server running at http://127.0.0.1:' + port);
 
 process.addListener('uncaughtException', function (err) {
